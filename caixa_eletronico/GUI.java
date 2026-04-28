0a5d56a946fe7c91f0697b7aa8b4f20538cb4fa7
Natalia Souza <93454080+natsouza24@users.noreply.github.com>
2026-04-28 10:28:11 -0300

    java
    Updated JOptionPane dialogs to include titles and message types for better user experience.

--- caixa_eletronico/GUI.java
+++ caixa_eletronico/GUI.java
@@ -41,6 +41,7 @@ public class GUI extends JFrame {
 		setTitle("Caixa Eletrônico");
 		setIconImage(Toolkit.getDefaultToolkit().getImage(GUI.class.getResource("/images/icone-java.png")));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 		setBounds(100, 100, 280, 380);
 		setResizable(false);
 		setLocationRelativeTo(null);
@@ -103,29 +104,38 @@ public class GUI extends JFrame {
 		
 		
 		/* EVENTOS */
+		
+		/* SAQUE */
 		btnSaque.addActionListener(e -> {
-		    String valor = JOptionPane.showInputDialog("Digite o valor do saque:");
+		    String valor = JOptionPane.showInputDialog(null,
+		    		"Digite o valor do saque:", "Saque", JOptionPane.QUESTION_MESSAGE);
 
 		    if (valor != null) {
 		        JOptionPane.showMessageDialog(null,
-		            caixa.sacar(Integer.parseInt(valor)));
+		            caixa.sacar(Integer.parseInt(valor)), "Saque", JOptionPane.INFORMATION_MESSAGE);
 		    }
 		});
 		
+		
+		/* RELATÓRIO */
         btnRelatorio.addActionListener(e -> {
             JOptionPane.showMessageDialog(null,
-             caixa.pegaRelatorioCedulas());
+             caixa.pegaRelatorioCedulas(), "Relatório de Cédulas", JOptionPane.INFORMATION_MESSAGE);
         });
         
+        
+        /* VALOR TOTAL */
         btnValorTotal.addActionListener(e -> {
             JOptionPane.showMessageDialog(null,
-                caixa.pegaValorTotalDisponivel());
+                caixa.pegaValorTotalDisponivel(), "Valor Total Disponível", JOptionPane.INFORMATION_MESSAGE);
         });
         
-
+        
+        /* REPOSIÇÃO */
         btnReposicao.addActionListener(e -> {
             try {
-                String ced = JOptionPane.showInputDialog("Digite a cédula (2, 5, 10, 20, 50 ou 100 reais):");
+                String ced = JOptionPane.showInputDialog(null,
+                		"Digite a cédula (2, 5, 10, 20, 50 ou 100 reais):", "Reposição de Cédulas", JOptionPane.QUESTION_MESSAGE);
 
                 if (ced == null) return;
 
@@ -135,45 +145,50 @@ public class GUI extends JFrame {
                 if (cedula != 2 && cedula != 5 && cedula != 10 &&
                     cedula != 20 && cedula != 50 && cedula != 100) {
 
-                    JOptionPane.showMessageDialog(null, "Cédula inválida!");
+                    JOptionPane.showMessageDialog(null,
+                    		"Cédula inválida!", "Reposição de Cédulas", JOptionPane.ERROR_MESSAGE);
                     return; // PARA AQUI
                 }
 
-                String qtd = JOptionPane.showInputDialog("Digite a quantidade:");
+                String qtd = JOptionPane.showInputDialog(null,
+                		"Digite a quantidade:", "Reposição de Cédulas", JOptionPane.QUESTION_MESSAGE);
 
                 if (qtd == null) return;
 
                 int quantidade = Integer.parseInt(qtd);
 
                 JOptionPane.showMessageDialog(null,
-                    caixa.reposicaoCedulas(cedula, quantidade)
-                );
+                		caixa.reposicaoCedulas(cedula, quantidade), "Reposição de Cédulas", JOptionPane.INFORMATION_MESSAGE);
 
             } catch (NumberFormatException ex) {
-                JOptionPane.showMessageDialog(null, "Digite apenas números!");
+                JOptionPane.showMessageDialog(null,
+                		"Digite apenas números!", "Reposição de Cédulas", JOptionPane.ERROR_MESSAGE);
             }
         });
         
         
+        /* COTA MINÍMA */
         btnCotaMinima.addActionListener(e -> {
             try {
-                String c = JOptionPane.showInputDialog("Digite a cota mínima:");
-                if (c != null) {
+                String cota = JOptionPane.showInputDialog(null,
+                		"Digite a cota mínima:", "Cota miníma", JOptionPane.QUESTION_MESSAGE);
+                if (cota != null) {
                     JOptionPane.showMessageDialog(null,
-                            caixa.armazenaCotaMinima(Integer.parseInt(c)));
+                            caixa.armazenaCotaMinima(Integer.parseInt(cota)), "Cota miníma", JOptionPane.INFORMATION_MESSAGE);
                 }
             } catch (Exception ex) {
-                JOptionPane.showMessageDialog(null, "Digite apenas números!");
+                JOptionPane.showMessageDialog(null, 
+                		"Digite apenas números!");
             }
         });
 
+        
+        /* EXTRATO */
         btnExtrato.addActionListener(e -> {
-
             JOptionPane.showMessageDialog(null,
-                ((CaixaEletronico) caixa).getExtrato()
-            );
+                ((CaixaEletronico) caixa).getExtrato(), "Extrato", JOptionPane.INFORMATION_MESSAGE);
 
-            System.exit(0);
+            System.exit(0); // Sair
         });	
 	}
 	
